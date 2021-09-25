951716cdf792aeac478948d977b2164d5a64a84e
thaontp106 <thaonguyen040694@gmail.com>
2021-09-25 08:46:13 +0700

    Buổi 18 - REGEX

--- src/main/java/RegexEx1.java
+++ src/main/java/RegexEx1.java
@@ -6,7 +6,7 @@ public class RegexEx1 {
     public static void main(String[] args) {
         System.out.print("Nhập chuỗi có chứa ngày tháng năm: ");
         String text1 = new Scanner(System.in).nextLine();
-        Pattern pattern = Pattern.compile("([0-2][0-9]|3[0-1])(/|-)(0[0-9]|1[0-2])(/|-)([0-9][0-9])?[0-9][0-9]");
+        Pattern pattern = Pattern.compile("([0-2][0-9]|3[0-1])[/-](0[0-9]|1[0-2])[/-]([0-9][0-9])?[0-9][0-9]");
         Matcher matcher = pattern.matcher(text1);
 
         System.out.format("Ngày tháng có định dạng dd-mm-yyyy hoặc dd/mm/yyyy trong chuỗi %s vừa nhập: \n", text1);
